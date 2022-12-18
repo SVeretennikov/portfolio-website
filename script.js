1ba9b936a60ac6eb08c81515e9bb55a31222aab8
SVeretennikov <serhii.veretennikov@gmail.com>
2022-12-18 21:05:25 +0000

    finished project. Fixed JS part so it changes coordinates of top panel changing states according to locations of sections on different screens

--- script.js
+++ script.js
@@ -11,18 +11,16 @@ const contactLoc = document.getElementById("contact");
 let navArr = [homeNav, skillsNav, projectsNav, contactNav];
 const clearNavs = () => {
     navArr.forEach(item => {
-        item.classList.remove("active")
+        item.classList.remove("active");
     });
 }
 
 const scrollToLoc = (loc, nav) => {
     loc.scrollIntoView({ behavior: "smooth", block: "start" });
-    nav.classList.add("active");
 }
 
 const scrollToHome = (nav) => {
     window.scroll({ behavior: "smooth", top: 0 });
-    nav.classList.add("active");
 }
 
 homeNav.addEventListener("click", () => scrollToHome(homeNav));
@@ -31,19 +29,43 @@ projectsNav.addEventListener("click", () => scrollToLoc(projectsLoc, projectsNav
 contactNav.addEventListener("click", () => scrollToLoc(contactLoc, contactNav));
 
 let scrollpos = window.scrollY;
+let windowWidth = window.innerWidth;
+
+const widthCoordinaresObj = {
+    99999: [2000, 1640, 900],
+    800: [2300, 1500, 700],
+    480: [2850, 2050, 1050]
+}
+
+let coordsArr = widthCoordinaresObj["99999"];
+if (windowWidth <= 800) {
+    coordsArr = widthCoordinaresObj["800"];
+} else if (windowWidth <= 480) {
+    coordsArr = widthCoordinaresObj["480"];
+}
+
+window.addEventListener("resize", () => {
+    windowWidth = window.innerWidth;
+    if (windowWidth <= 480) {
+        coordsArr = widthCoordinaresObj["480"];
+    } else if (windowWidth <= 800) {
+        coordsArr = widthCoordinaresObj["800"];
+    } else {
+        coordsArr = widthCoordinaresObj["99999"];
+    }
+})
 
 window.addEventListener('scroll', function() { 
     scrollpos = window.scrollY;
 
     clearNavs();
-    if (scrollpos >= 2200) {
+    if (scrollpos >= coordsArr[0]) {
         contactNav.classList.add("active");
-    } else if (scrollpos >= 1600) {
+    } else if (scrollpos >= coordsArr[1]) {
         projectsNav.classList.add("active");
-    } else if (scrollpos >= 500) {
+    } else if (scrollpos >= coordsArr[2]) {
         skillsNav.classList.add("active");
     } else {
-        homeNav.classList.add("active") 
+        homeNav.classList.add("active");
     }
-
 })
